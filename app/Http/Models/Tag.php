ee101a7817ff7b393b2071fd6eb52f7c64011d5c
olsososo <olsososo@gmail.com>
2016-11-11 16:16:26 +0000

    添加模型

---
+++ app/Http/Models/Tag.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tag extends Model
+{
+    /**
+     * @var bool
+     */
+    public $timestamps = false;
+}
---
+++ app/Http/Models/Torrent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Torrent extends Model
+{
+    /**
+     * @var bool
+     */
+    public $timestamps = false;
+}
