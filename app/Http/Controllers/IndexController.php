af4862db8959e1247ea504ede92841b7892291c1
olsososo <olsososo@gmail.com>
2016-11-12 02:06:03 +0800

    '首页'

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
-    //
+    /**
+     * 首页
+     */
+    public function index()
+    {
+        return view('index.index');
+    }
 }
