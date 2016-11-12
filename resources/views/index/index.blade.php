3269bbb3dd194862ff35357e4d862f2db6f4676a
olsososo <olsososo@gmail.com>
2016-11-12 22:36:40 +0800

    'bootstrap'

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -3,29 +3,21 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Bootstrap 101 Template</title>
+        <title>BT</title>
         
-        <!-- 新 Bootstrap 核心 CSS 文件 -->
-        <link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/3.3.0/css/bootstrap.min.css">
+<!--         新 Bootstrap 核心 CSS 文件 
+        <link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/3.3.0/css/bootstrap.min.css">-->
         <link rel="stylesheet" href="{{ URL::asset('css/index.css') }}" >
     </head>
     <body>
-        <div class="container-fluid">
-            <div class="row-fluid">
-                <div class="col-md-12">
-                    <ul class="menu_ul">
-                        <li><a href="">Wikipadia</a></li>
-                        <li><a href="">捐赠</a></li>
-                        <li><a href="">登录</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>        
+        <div class="menu">
+            
+        </div>
 
-        <!-- jQuery文件。务必在bootstrap.min.js 之前引入 -->
+<!--         jQuery文件。务必在bootstrap.min.js 之前引入 
         <script src="http://cdn.bootcss.com/jquery/1.11.1/jquery.min.js"></script>
 
-        <!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
-        <script src="http://cdn.bootcss.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>        
+         最新的 Bootstrap 核心 JavaScript 文件 
+        <script src="http://cdn.bootcss.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>        -->
     </body>
 </html>
