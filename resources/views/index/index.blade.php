e7b359e9a150bc7c293272ccadbdab228a42af83
olsososo <olsososo@gmail.com>
2016-11-12 21:32:07 +0800

    'bootstrap'

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -12,9 +12,9 @@
     <body>
         <div class="container-fluid">
             <div class="row">
-                <div class="col-md-10">.col-md-8</div>
-                <div class="col-md-1"></div>
-                <div class="col-md-1"></div>
+                <div class="col-md-10">.col-md-10</div>
+                <div class="col-md-1">.col-md-1</div>
+                <div class="col-md-1">.col-md-1</div>
             </div>
         </div>        
 
