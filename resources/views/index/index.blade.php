35027219175022a7b919d61576b0f96cb348cbd1
olsososo <olsososo@gmail.com>
2016-11-12 21:24:24 +0800

    'bootstrap'

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -1,5 +1,21 @@
-<form action="{{url('/search')}}" method="POST">
-    {!! csrf_field() !!}
-    <input type="text" name="keyword" />
-    <input type="submit" value="search" />
-</form>
+<!DOCTYPE html>
+<html lang="zh-cn">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Bootstrap 101 Template</title>
+        
+        <!-- 新 Bootstrap 核心 CSS 文件 -->
+        <link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/3.3.0/css/bootstrap.min.css">
+        <link rel="stylesheet" href="{{ URL::asset('css/index.css'); }}" >
+    </head>
+    <body>
+        
+
+        <!-- jQuery文件。务必在bootstrap.min.js 之前引入 -->
+        <script src="http://cdn.bootcss.com/jquery/1.11.1/jquery.min.js"></script>
+
+        <!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
+        <script src="http://cdn.bootcss.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>        
+    </body>
+</html>
