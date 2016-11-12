ae8ae30b178d5e42fcb593c878d7a3a2df4f1ae8
olsososo <olsososo@gmail.com>
2016-11-13 01:31:36 +0800

    'tpl'

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -17,20 +17,6 @@
             </div>
         </div>
         
-        <div class id="lga">
-            <img src="http://7xtf51.com2.z0.glb.clouddn.com/engiy/logo.png">
-            <div id="logo-sub">简体中文</div>
-        </div>
-        
-        <div id="fkbx">
-            <form action="#" id="f" method="get">
-                <input type="text" name="q" id="kw"/><br/>
-                <input type="submit" value="搜索" id="jsb"/>
-            </form>
-        </div>
-        
-        <div id="footer">
-            我是底部
-        </div>
+
     </body>
 </html>
