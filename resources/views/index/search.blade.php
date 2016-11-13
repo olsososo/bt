4089d748f82cf264557af13f97b85a6e284e2bba
olsososo <olsososo@gmail.com>
2016-11-13 14:40:34 +0800

    'tpl'

--- resources/views/index/search.blade.php
+++ resources/views/index/search.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>BT</title>
+        <link rel="stylesheet" href="{{ URL::asset('css/global.css') }}" >
         <link rel="stylesheet" href="{{ URL::asset('css/search.css') }}" >
     </head>
     <body>
