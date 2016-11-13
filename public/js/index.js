6c8ede472cd8d478cc1c3244b3dcb37a8a5f1bb2
olsososo <olsososo@gmail.com>
2016-11-13 14:14:57 +0800

    'tpl'

--- public/js/index.js
+++ public/js/index.js
@@ -2,7 +2,7 @@ window.onload = function() {
     var search = document.getElementById('search');
     var form = document.getElementById('f');
     search.onclick = function() {
-        var keyword = document.getElementById('keyword');
+        var keyword = document.getElementById('keyword').value;
         location.href = form.getAttribute('action') + keyword;
     };
 };
