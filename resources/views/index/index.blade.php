fd16c27bfd1f37d1ae118cbbc7963f829908aa2b
olsososo <olsososo@gmail.com>
2016-11-13 13:31:00 +0800

    'tpl'

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -32,15 +32,17 @@
         </div>
         
         <div id="footer">
-            <span id="copyright">
-                本站仅供测试和学习交流, 内容均收集于互联网，如果有侵权内容、不妥之处，请联系我们删除。敬请谅解！
-            </span> 
-            
-            <select id="language">
-                <option>English</option>
-                <option>简体中文</option>
-                <option>繁体中文</option>
-            </select>
+            <div id="footer-sub">
+                <span id="copyright">
+                    本站仅供测试和学习交流, 内容均收集于互联网，如果有侵权内容、不妥之处，请联系我们删除。敬请谅解！
+                </span> 
+
+                <select id="language">
+                    <option>English</option>
+                    <option>简体中文</option>
+                    <option>繁体中文</option>
+                </select>
+            </div>
         </div>
     </body>
 </html>
