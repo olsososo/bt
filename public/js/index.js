5caff80e67d36f24ba02a20e8579f54b310a6633
olsososo <olsososo@gmail.com>
2016-11-13 15:22:10 +0800

    'tpl'

--- public/js/index.js
+++ public/js/index.js
@@ -1,19 +1,14 @@
-window.onload = function() {
-    var search = document.getElementById('search');
+$(document).ready(function(){
+    $('#search').click(function(){
+        var keyword = $('#keyword').val();
+        var action = $('#form').attr('action');
+        location.href = action + '/' + keyword;           
+    });
     
-    function next() {
-        var keyword = document.getElementById('keyword').value;
-        var form = document.getElementById('form');
-        location.href = form.getAttribute('action') + '/' + keyword;      
-    }
-    
-    document.onkeydown = function(event){
-        event = document.all ? window.event : event;
-        if((event.keyCode || event.which) == 13){
-            next();
+    $(document).keydown(function(event){
+        if(event.keyCode ==13){
+            $('#search').trigger('click');
         }
-    }; 
-   
-    search.onclick = next;
-};
+    });
+});
 
