503592533e79ac22375610f89f1eeb4040ec61c2
olsososo <olsososo@gmail.com>
2016-11-13 14:32:12 +0800

    'tpl'

---
+++ resources/views/index/search.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="zh-cn">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>BT</title>
+        <link rel="stylesheet" href="{{ URL::asset('css/search.css') }}" >
+    </head>
+    <body>
+        <div id="header">
+            <div id="logo">
+                
+            </div>
+            
+            <div id="menu">
+                <ul>
+                    <li><a href="#">工具</a></li>
+                    <li><a href="#">教程</a></li>
+                    <li><a href="#">加入收藏</a></li>
+                </ul>             
+            </div>
+        </div>
+    </body>
+</html>
