bbfa6573ec0e2ea1eea625d5951416bbf5dcf7b0
olsososo <olsososo@gmail.com>
2016-11-13 11:17:49 +0800

    'tpl'

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -18,9 +18,9 @@
         </div>
         
         <div id="logo">
-            <div>
+            <div id="logo-sub">
                 <img src="https://g.hancel.net/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png">
-                <div id="logo-sub">简体中文</div>                
+                <div id="logo-title">简体中文</div>                
             </div>
         </div>
         
