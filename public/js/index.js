cbcf7616d6ff457350f06346a2ade5e332a8457c
olsososo <olsososo@gmail.com>
2016-11-13 15:13:02 +0800

    'tpl'

--- public/js/index.js
+++ public/js/index.js
@@ -1,23 +1,19 @@
 window.onload = function() {
     var search = document.getElementById('search');
     
-    function search() {
-        var keyword = document.getElementById('keyword').value;
-        var action = document.getElementById('action').value;
-        location.href = action + '/' + keyword;       
-    }
-    
-    document.keydown(function(event){
-        event=document.all ? window.event : event;
-        if((event.keyCode || event.which) == 13){
-            alert(333);
-        }
-    }); 
-   
-    search.onclick = function() {
+    function next() {
         var keyword = document.getElementById('keyword').value;
         var form = document.getElementById('form');
-        location.href = form.getAttribute('action') + '/' + keyword;
-    };
+        location.href = form.getAttribute('action') + '/' + keyword;      
+    }
+    
+    document.onkeydown = function(event){
+        event = document.all ? window.event : event;
+        if((event.keyCode || event.which) == 13){
+            next();
+        }
+    }; 
+   
+    search.onclick = next;
 };
 
