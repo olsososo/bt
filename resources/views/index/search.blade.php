20127764dce9aa39932b056f8eacce66c58b3e53
olsososo <olsososo@gmail.com>
2016-11-15 01:20:06 +0800

    'tpl'

--- resources/views/index/search.blade.php
+++ resources/views/index/search.blade.php
@@ -8,33 +8,7 @@
         <link rel="stylesheet" href="{{ URL::asset('css/search.css') }}" >
     </head>
     <body>
-        <div id="header">
-           <div id="left_menu">
-                <table cellspacing="0" cellpadding="0">
-                    <tr>
-                        <td id="logo">
-                            <a href="#"><img src="http://7xtf51.com2.z0.glb.clouddn.com/engiy/logo.png" /></a>
-                        </td>
-                        <td>
-                            <input type="text" id="keyword"/>
-                        </td>
-                        <td>
-                            <span id="search_spn">
-                                <input id="search_btn" type="button" value="搜索" />
-                            </span>
-                        </td>
-                    </tr>
-                </table>
-            </div>
-            
-            <div id="menu">
-                <ul>
-                    <li><a href="#">工具</a></li>
-                    <li><a href="#">教程</a></li>
-                    <li><a href="#">加入收藏</a></li>
-                </ul>             
-            </div>
-        </div>
+        @include('header')
         
         <div id="container">
             <div id="result_stats">
