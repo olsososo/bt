dcb9cad8e606e3263ea9d559fbd12d653a57c7d0
olsososo <olsososo@gmail.com>
2016-11-15 01:32:12 +0800

    'tpl'

--- resources/views/index/show.blade.php
+++ resources/views/index/show.blade.php
@@ -8,20 +8,7 @@
         <link rel="stylesheet" href="{{ URL::asset('css/show.css') }}" >
     </head>
     <body>
-        <div id="header">
-           <div id="logo">
-                <a href="#"><img src="http://7xtf51.com2.z0.glb.clouddn.com/engiy/logo.png" /></a>
-                <input type="text" id="keyword"/>
-            </div>
-            
-            <div id="menu">
-                <ul>
-                    <li><a href="#">工具</a></li>
-                    <li><a href="#">教程</a></li>
-                    <li><a href="#">加入收藏</a></li>
-                </ul>             
-            </div>
-        </div>
+        @include('header')
         
         <div id="container">
             <div id="summary">
@@ -112,19 +99,7 @@
             </div>            
         </div>
         
-        <div id="footer">
-            <div id="footer-sub">
-                <span id="copyright">
-                    本站仅供测试和学习交流, 内容均收集于互联网，如果有侵权内容、不妥之处，请联系我们删除。敬请谅解！
-                </span> 
-
-                <select id="language">
-                    <option>简体中文</option>
-                    <option>繁体中文</option>
-                    <option>English</option>
-                </select>
-            </div>
-        </div>
+        @include('footer')
     </body>
 </html>
         
