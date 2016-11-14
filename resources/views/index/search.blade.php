9fd01872419fbd910d4a05fffa2cda75384e8d99
olsososo <olsososo@gmail.com>
2016-11-15 01:21:53 +0800

    'tpl'

--- resources/views/index/search.blade.php
+++ resources/views/index/search.blade.php
@@ -41,18 +41,6 @@
             </div>            
         </div>
         
-        <div id="footer">
-            <div id="footer-sub">
-                <span id="copyright">
-                    本站仅供测试和学习交流, 内容均收集于互联网，如果有侵权内容、不妥之处，请联系我们删除。敬请谅解！
-                </span> 
-
-                <select id="language">
-                    <option>简体中文</option>
-                    <option>繁体中文</option>
-                    <option>English</option>
-                </select>
-            </div>
-        </div>
+        @include('footer')
     </body>
 </html>
