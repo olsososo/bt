8ebc7ce85b35aab3d32e2f8be898ca8a106639c1
olsososo <olsososo@gmail.com>
2016-11-14 17:41:21 +0800

    'tpl'

--- resources/views/index/show.blade.php
+++ resources/views/index/show.blade.php
@@ -57,8 +57,20 @@
                 </dl>
             </div>
             
-            <div id="files">
+            <div id="detail">
+                <div id='detail_sub'>
+                    磁力链接详情
+                </div>
                 
+                <dl>
+                    <dt>Hash值</dt><dd>3D4C0F51FE483E97B25FCF6B9ECABBCADB826EF1</dd>
+                    <dt>Hash值</dt><dd>3D4C0F51FE483E97B25FCF6B9ECABBCADB826EF1</dd>
+                    <dt>Hash值</dt><dd>3D4C0F51FE483E97B25FCF6B9ECABBCADB826EF1</dd>
+                    <dt>Hash值</dt><dd>3D4C0F51FE483E97B25FCF6B9ECABBCADB826EF1</dd>
+                    <dt>Hash值</dt><dd>3D4C0F51FE483E97B25FCF6B9ECABBCADB826EF1</dd>
+                    <dt>Hash值</dt><dd>3D4C0F51FE483E97B25FCF6B9ECABBCADB826EF1</dd>
+                    <dt>Hash值</dt><dd>3D4C0F51FE483E97B25FCF6B9ECABBCADB826EF1</dd>
+                </dl>
             </div>
         </div>
         
