b4cd18ed13ecd08cc03c6c679923dc17e5a430b8
olsososo <olsososo@gmail.com>
2016-11-16 11:46:50 +0800

    '修改'

--- resources/views/index/show.blade.php
+++ resources/views/index/show.blade.php
@@ -11,101 +11,95 @@
     <body>
         @include('header')
         
-        <div id="main">
-            <div id="container">
-                <div id="summary">
-                    <a class="title" href="{{ URL::route('show', ['id'=>base64_encode($torrent['id'])]) }}">
-                        {{ $torrent['name'] }}
-                    </a><br/>
-
-                    <a class="magnet" href="magnet:?xt=urn:btih:{{ $torrent['infohash']}}">
-                        magnet:?xt=urn:btih:{{ $torrent['infohash']}}
-                    </a><br/>
-
-                    <ul>
-                        <li class="copy"><a href='#'>复制链接</a></li>
-                        <li class='download'><a href='magnet:?xt=urn:btih:{{ $torrent['infohash']}}'>磁力下载</a></li>
-                        <li class='play'><a href='http://www.lixianhezi.com/yun/' target="_blank">网盘播放</a></li>
-                    </ul>
-                </div>
-
-                <div class="panel panel-success">
-                    <div class="panel-heading">
-                    <h3 class="panel-title">磁链详情</h3>
-                    </div>
-                    <div class="panel-body">
-                        <table class='torrent'>
-                            <tr>
-                                <td class="left">Hash值:</td>
-                                <td class="right">{{ $torrent['infohash'] }}</td>
-                            </tr>
-
-                            <tr>
-                                <td class="left">种子热度:</td>
-                                <td class="right">{{ $torrent['hits'] }}</td>
-                            </tr>
-
-                            <tr>
-                                <td class="left">文件大小:</td>
-                                <td class="right">{{ $torrent['length'] }}</td>
-                            </tr>    
-
-                            <tr>
-                                <td class="left">文件数量:</td>
-                                <td class="right">{{ count($files) }}</td>
-                            </tr>   
-
-                            <tr>
-                                <td class="left">创建日期:</td>
-                                <td class="right">{{ $torrent['created_at'] }}</td>
-                            </tr>   
-
-                            <tr>
-                                <td class="left">最后访问:</td>
-                                <td class="right">{{ $torrent['created_at'] }}</td>
-                            </tr>   
-
-                            <tr>
-                                <td class="left">访问标签:</td>
-                                <td class="right">
-                                    @foreach($tags as $tag)
-                                    <span class="tag"><a href='{{ URL::route('search', ['keyword'=>$tag->tag]) }}'>{{ $tag->tag }}</a></span>
-                                    @endforeach
-                                </td>
-                            </tr>                       
-                        </table>
-                      </div>
-                </div>
-
-                <div class="panel panel-info">
-                      <div class="panel-heading">
-                        <h3 class="panel-title">文件列表</h3>
-                      </div>
-                      <div class="panel-body">
-                            <table class='file'>
-                                @foreach($files as $file)
-                                <tr>
-                                    <td class="left">{{ $file->file}}</td>
-                                    <td class="right">{{ $file->length }}</td>
-                                </tr>
-                                @endforeach
-                            </table>
-                      </div>
-                </div>
-
-                <div class="panel panel-danger">
-                      <div class="panel-heading">
-                        <h3 class="panel-title">本站声明</h3>
-                      </div>
-                      <div class="panel-body">
-                            <span class="notice">本站仅提供交流分享BT下载/迅雷下载/电驴下载等有关.torrentBT种子文件的信息，其他实际文件均不在本站服务器，如果您认为有资源信息侵犯了您的权益，请告知本站，我们将尽快协助处理。</span>
-                      </div>
-                </div>     
-            </div> 
-            
-            <div id="guide">
-                asdddddddd
+        <div id="container">
+            <div id="summary">
+                <a class="title" href="{{ URL::route('show', ['id'=>base64_encode($torrent['id'])]) }}">
+                    {{ $torrent['name'] }}
+                </a><br/>
+                       
+                <a class="magnet" href="magnet:?xt=urn:btih:{{ $torrent['infohash']}}">
+                    magnet:?xt=urn:btih:{{ $torrent['infohash']}}
+                </a><br/>
+                
+                <ul>
+                    <li class="copy"><a href='#'>复制链接</a></li>
+                    <li class='download'><a href='magnet:?xt=urn:btih:{{ $torrent['infohash']}}'>磁力下载</a></li>
+                    <li class='play'><a href='http://www.lixianhezi.com/yun/' target="_blank">网盘播放</a></li>
+                </ul>
             </div>
+            
+            <div class="panel panel-success">
+                <div class="panel-heading">
+                <h3 class="panel-title">磁链详情</h3>
+                </div>
+                <div class="panel-body">
+                    <table class='torrent'>
+                        <tr>
+                            <td class="left">Hash值:</td>
+                            <td class="right">{{ $torrent['infohash'] }}</td>
+                        </tr>
+
+                        <tr>
+                            <td class="left">种子热度:</td>
+                            <td class="right">{{ $torrent['hits'] }}</td>
+                        </tr>
+
+                        <tr>
+                            <td class="left">文件大小:</td>
+                            <td class="right">{{ $torrent['length'] }}</td>
+                        </tr>    
+
+                        <tr>
+                            <td class="left">文件数量:</td>
+                            <td class="right">{{ count($files) }}</td>
+                        </tr>   
+
+                        <tr>
+                            <td class="left">创建日期:</td>
+                            <td class="right">{{ $torrent['created_at'] }}</td>
+                        </tr>   
+
+                        <tr>
+                            <td class="left">最后访问:</td>
+                            <td class="right">{{ $torrent['created_at'] }}</td>
+                        </tr>   
+
+                        <tr>
+                            <td class="left">访问标签:</td>
+                            <td class="right">
+                                @foreach($tags as $tag)
+                                <span class="tag"><a href='{{ URL::route('search', ['keyword'=>$tag->tag]) }}'>{{ $tag->tag }}</a></span>
+                                @endforeach
+                            </td>
+                        </tr>                       
+                    </table>
+                  </div>
+            </div>
+            
+            <div class="panel panel-info">
+                  <div class="panel-heading">
+                    <h3 class="panel-title">文件列表</h3>
+                  </div>
+                  <div class="panel-body">
+                        <table class='file'>
+                            @foreach($files as $file)
+                            <tr>
+                                <td class="left">{{ $file->file}}</td>
+                                <td class="right">{{ $file->length }}</td>
+                            </tr>
+                            @endforeach
+                        </table>
+                  </div>
+            </div>
+            
+            <div class="panel panel-danger">
+                  <div class="panel-heading">
+                    <h3 class="panel-title">本站声明</h3>
+                  </div>
+                  <div class="panel-body">
+                        <span class="notice">本站仅提供交流分享BT下载/迅雷下载/电驴下载等有关.torrentBT种子文件的信息，其他实际文件均不在本站服务器，如果您认为有资源信息侵犯了您的权益，请告知本站，我们将尽快协助处理。</span>
+                  </div>
+            </div>     
         </div>
         
         @include('footer')
