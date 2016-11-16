dca992ce161e70c8db383851ce50e111242ee7b6
olsososo <olsososo@gmail.com>
2016-11-16 16:35:50 +0800

    'tpl'

--- resources/views/about/tutorial.blade.php
+++ resources/views/about/tutorial.blade.php
@@ -4,9 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>BT</title>
-        <link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/3.3.0/css/bootstrap.min.css">
-        <link rel="stylesheet" href="{{ URL::asset('css/global.css') }}" >        
-        <link rel="stylesheet" href="{{ URL::asset('css/statement.css') }}" >  
+        <link rel="stylesheet" target='_blank' href="http://cdn.bootcss.com/bootstrap/3.3.0/css/bootstrap.min.css">
+        <link rel="stylesheet" target='_blank' href="{{ URL::asset('css/global.css') }}" >        
+        <link rel="stylesheet" target='_blank' href="{{ URL::asset('css/statement.css') }}" >  
         <style>
             #footer {
                 position: fixed;
@@ -22,10 +22,9 @@
                 <div class="col-md-9" role='main'>
                     <div class="bs-docs-section">
                         <h1 id="dropdowns" class="page-header">使用教程</h1>
-                        <p>{{ $site_name }}是一个<a href='http://zh.wikipedia.org/zh/%E7%A3%81%E5%8A%9B%E9%93%BE%E6%8E%A5' target="_blank">磁力链接</a>搜索引擎，是学术研究的副产品，用于解决资源过度分散的问题</p>
-                        <p>它通过<a href="http://zh.wikipedia.org/zh/BitTorrent_(%E5%8D%8F%E8%AE%AE)">BitTorrent协议</a>加入DHT网络，实时的自动采集数据，仅存储文件的标题、大小、文件列表、文件标识符（磁力链接）等基础信息</p>
-                        <p>{{ $site_name }}不下载任何真实资源，无法判断资源的合法性及真实性，使用{{ $site_name }}服务的用户需自行鉴别内容的真伪</p>
-                        <p>{{ $site_name }}不上传任何资源，不提供<a href="http://zh.wikipedia.org/zh/BitTorrent_tracker">Tracker服务</a>，不提供<a href="http://zh.wikipedia.org/wiki/%E7%A7%8D%E5%AD%90%E6%96%87%E4%BB%B6">种子文件</a>的下载，这意味着{{ $site_name }}是一个完全合法的系统</p>
+                        <p>{{ $site_name }}返回的结果是磁力链接，这并不意味着您能通过{{ $site_name }}直接下载资源，您需要通过下载工具才能实现下载</p>
+                        <p>目前主流的下载工具有：<a target='_blank' href="http://xunlei.com/">迅雷</a>、<a target='_blank' href="http://xf.qq.com/">QQ旋风</a>、<a target='_blank' href='http://www.utorrent.com/'>μTorrent</a>、<a target='_blank' href="http://www.bitcomet.com/">BitComet</a>等</p>
+                        <p>在线看电影请复制磁力链接，使用<a target='_blank' href='http://115.com/'>115离线下载</a>，<a target='_blank' href='http://pan.baidu.com/'>百度网盘离线下载</a>或<a target='_blank' href="http://xunlei.com/">迅雷边下边看</a>。</p>
                     </div>
                 </div>
             </div>
