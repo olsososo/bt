7f548d14e45e83f1dd02ccb70caea793a97e1ae3
olsososo <olsososo@gmail.com>
2016-11-16 15:50:36 +0800

    '修改'

--- resources/views/about/statement.blade.php
+++ resources/views/about/statement.blade.php
@@ -4,6 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>BT</title>
+        <link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/3.3.0/css/bootstrap.min.css">
+        <link rel="stylesheet" href="{{ URL::asset('css/global.css') }}" >        
         <link rel="stylesheet" href="{{ URL::asset('css/statement.css') }}" >  
     </head>
     <body>
