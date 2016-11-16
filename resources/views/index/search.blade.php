62df7824081699ed510dc847144bb984a367afd8
olsososo <olsososo@gmail.com>
2016-11-16 13:07:00 +0800

    '修改'

--- resources/views/index/search.blade.php
+++ resources/views/index/search.blade.php
@@ -21,7 +21,7 @@
                     @foreach($torrents as $torrent)
                     <li>
                         <p><a class="title" target="_blank" href="{{ URL::route('show', ['id'=>base64_encode($torrent->id)]) }}">
-                            {{ $torrent->name }}</a>
+                            {{ preg_replace($keyword, "<span style='color: #dd4b39;'>$keyword</span>"", $torrent->name) }}</a>
                         </p>
                         <span class='st'>
                             文件大小: <span class="label label-success">{{ $torrent->length }}</span>
