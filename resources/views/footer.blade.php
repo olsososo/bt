77a69f4c1ef26aef0fe95c5cdcd89570bcd533da
olsososo <olsososo@gmail.com>
2016-11-16 17:24:51 +0800

    'tpl'

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -5,7 +5,7 @@
                         Copyright © 2016 - 2018 {{ $site_name }}
                     </p> 
                     
-                    <p class="copyright">
+                    <p class="copyright" style="margin-top: 10px;">
                         声明：{{ $site_name }}（{{ url('/') }}）仅实时展示DHT网络动态，不提供任何BT种子和资源文件下载！
                     </p>                     
                 </div>
