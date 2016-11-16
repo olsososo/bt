5ced17129a6ae584dbf13954e9785362bd2be1a1
olsososo <olsososo@gmail.com>
2016-11-16 15:49:34 +0800

    '修改'

--- resources/views/about/statement.blade.php
+++ resources/views/about/statement.blade.php
@@ -1,8 +1,18 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<!DOCTYPE html>
+<html lang="zh-cn">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>BT</title>
+        <link rel="stylesheet" href="{{ URL::asset('css/statement.css') }}" >  
+    </head>
+    <body>
+        @include('header')
+        
+        @include('footer')
+        <script src="http://cdn.bootcss.com/jquery/1.11.1/jquery.min.js"></script>
+        <script src="http://cdn.bootcss.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>   
+        <script type="text/javascript" src="http://v3.jiathis.com/code/jiathis_r.js?move=0&amp;btn=r1.gif" charset="utf-8"></script>
+        <script type="text/javascript" src="{{ URL::asset('js/search.js') }}"></script> 
+    </body>
+</html>
