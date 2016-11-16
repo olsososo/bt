e11452b16e8e93f3a5a5937713baff4fd20d4366
olsososo <olsososo@gmail.com>
2016-11-16 16:51:02 +0800

    'tpl'

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,9 +1,15 @@
         <div id="footer">
             <div id="footer-sub">
-                <span id="copyright">
-                    声明：{{ $site_name }}（{{ url('/') }}）仅实时展示DHT网络动态，不提供任何BT种子和资源文件下载！
-                </span> 
-
+                <div>
+                    <span>
+                        声明：{{ $site_name }}（{{ url('/') }}）仅实时展示DHT网络动态，不提供任何BT种子和资源文件下载！
+                    </span> 
+                    
+                    <span id="copyright">
+                        声明：{{ $site_name }}（{{ url('/') }}）仅实时展示DHT网络动态，不提供任何BT种子和资源文件下载！
+                    </span>                     
+                </div>
+                
                 <select id="language">
                     <option>简体中文</option>
                     <option>繁体中文</option>
