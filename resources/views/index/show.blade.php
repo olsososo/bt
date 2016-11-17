9046113988f5da422a473594354c7a80005da51c
olsososo <olsososo@gmail.com>
2016-11-17 22:05:21 +0800

    'commit'

--- resources/views/index/show.blade.php
+++ resources/views/index/show.blade.php
@@ -45,7 +45,19 @@
                 <div class="col-md-9" role='main'>
                     <div class="bs-docs-section">
                         <h3 id="dropdowns" class="page-header">磁链详情</h3>
-
+<div class="bs-docs-example">
+          <dl class="dl-horizontal">
+            <dt>Description lists</dt>
+            <dd>A description list is perfect for defining terms.</dd>
+            <dt>Euismod</dt>
+            <dd>Vestibulum id ligula porta felis euismod semper eget lacinia odio sem nec elit.</dd>
+            <dd>Donec id elit non mi porta gravida at eget metus.</dd>
+            <dt>Malesuada porta</dt>
+            <dd>Etiam porta sem malesuada magna mollis euismod.</dd>
+            <dt>Felis euismod semper eget lacinia</dt>
+            <dd>Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</dd>
+          </dl>
+        </div>
                     </div>
                 </div>
             </div>
