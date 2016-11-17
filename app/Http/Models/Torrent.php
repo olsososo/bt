19496e770e18abbcae59b34f18954ac4e5bd0331
olsososo <olsososo@gmail.com>
2016-11-17 23:49:22 +0800

    'commit'

--- app/Http/Models/Torrent.php
+++ app/Http/Models/Torrent.php
@@ -6,25 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class Torrent extends Model
 {
-    /**
-     * @var bool
-     */
-    public $timestamps = false;
-    
-    protected $casts = [
-        'status'    =>  'boolean',
-    ];
-    
-    public function getCreatedAtAttribute($value)
-    {
-        return date('Y-m-d', $value);
-    }
-    
-    public function getLengthAttribute($value)
-    {
-        $s = array('B', 'Kb', 'MB', 'GB', 'TB', 'PB');
-        $e = floor(log($value) / log(1024));
-
-        return sprintf('%.2f '.$s[$e], ($value / pow(1024, $e)));       
-    }
+//    /**
+//     * @var bool
+//     */
+//    public $timestamps = false;
+//    
+//    protected $casts = [
+//        'status'    =>  'boolean',
+//    ];
+//    
+//    public function getCreatedAtAttribute($value)
+//    {
+//        return date('Y-m-d', $value);
+//    }
+//    
+//    public function getLengthAttribute($value)
+//    {
+//        $s = array('B', 'Kb', 'MB', 'GB', 'TB', 'PB');
+//        $e = floor(log($value) / log(1024));
+//
+//        return sprintf('%.2f '.$s[$e], ($value / pow(1024, $e)));       
+//    }
 }
