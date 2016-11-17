3dd4f57efbf7126b7d0fcf8941118e5080a2539c
olsososo <olsososo@gmail.com>
2016-11-17 22:54:21 +0800

    'commit'

--- resources/views/index/hot.blade.php
+++ resources/views/index/hot.blade.php
@@ -11,31 +11,40 @@
     <body>
         @include('header')
         
-        <div id="container">
-            <div id="result_stats">
-                找到约 {{ number_format($total) }} 条结果 （用时 {{ number_format($running_time, 2) }} 秒）
+        <div class="container" style="margin-top: 20px;">
+            <div class="row">
+                <div class="col-md-9" role='main'>
+                    <p>
+                        找到约 {{ number_format($total) }} 条结果 （用时 {{ number_format($running_time, 2) }} 秒）
+                    </p>
+
+                    <div id="result">
+                        <ul>
+                            @foreach($torrents as $torrent)
+                            <li>
+                                <p><a class="title" target="_blank" href="{{ URL::route('show', ['id'=>base64_encode($torrent['id'])]) }}">
+                                    {{ $torrent['name'] }}</a>
+                                </p>
+                                <span class='st'>
+                                    文件大小: <span class="label label-success" style="margin-right: 10px;">{{ size_format($torrent['length']) }}</span>
+                                    创建时间: <span class="label label-primary" style="margin-right: 10px;">{{ date('Y-m-d', $torrent['created_at']) }}</span>
+                                    更新时间: <span class="label label-warning" style="margin-right: 10px;">{{ date('Y-m-d', $torrent['created_at']) }}</span>
+                                    资源热度: <span class="label label-danger" style="margin-right: 10px;">{{ $torrent['hits'] }}</span>                                
+                                </span>
+                            </li>
+                            @endforeach
+                        </ul>
+                    </div>
+                    
+                    <div id="page">
+                        <table style="height: 80px; width: 100%;">
+                            <tr>
+                                <td>{!! $pagintor->render() !!}</td>
+                            </tr>
+                        </table>
+                    </div>                      
+                </div>
             </div>
-            
-            <div id="result">
-                <ul>
-                    @foreach($torrents as $torrent)
-                    <li>
-                        <p>
-                            <span class="glyphicon glyphicon-fire" style="color: #FF0309; margin-right: 10px;"></span>
-                            <a class="title" target="_blank" href="{{ URL::route('show', ['id'=>base64_encode($torrent['id'])]) }}">
-                                {{ $torrent['name'] }}
-                            </a>
-                        </p>
-                        <span class='st'>
-                            文件大小: <span class="label label-success" style="margin-right: 10px;">{{ size_format($torrent['length']) }}</span>
-                            创建时间: <span class="label label-primary" style="margin-right: 10px;">{{ date('Y-m-d', $torrent['created_at']) }}</span>
-                            更新时间: <span class="label label-warning" style="margin-right: 10px;">{{ date('Y-m-d', $torrent['created_at']) }}</span>
-                            资源热度: <span class="label label-danger" style="margin-right: 10px;">{{ $torrent['hits'] }}</span>                                
-                        </span>
-                    </li>
-                    @endforeach
-                </ul>
-            </div>          
         </div>
         
         @include('footer')
