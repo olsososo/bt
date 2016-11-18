9cdf877fb6b3346e4e03e7f2c24707b25b8b923f
olsososo <olsososo@gmail.com>
2016-11-18 11:29:55 +0800

    'tpl'

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -9,11 +9,19 @@
                         声明：{{ $site_name }}（{{ url('/') }}）仅实时展示DHT网络动态，不提供任何BT种子和资源文件下载！
                     </p>                     
                 </div>
-                
-                <select id="language">
+               
+                <div class="btn-group">
+                  <button class="btn btn-default btn-sm dropdown-toggle" type="button" data-toggle="dropdown">
+                    Small button <span class="caret"></span>
+                  </button>
+                  <ul class="dropdown-menu" role="menu">
+                    ...
+                  </ul>
+                </div>                
+<!--                <select id="language">
                     <option>简体中文</option>
                     <option>繁体中文</option>
                     <option>English</option>
-                </select>
+                </select>-->
             </div>
         </div>
