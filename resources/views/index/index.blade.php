9e4c6d460a2718e0b26a09c9f828e6ceac8f372d
olsososo <olsososo@gmail.com>
2016-11-18 16:23:42 +0800

    'tpl'

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -20,8 +20,7 @@
         
         <div id="logo">
             <div id="logo-sub">
-                <img src="https://g.hancel.net/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png">
-                <div id="logo-title">{{ trans('messages.locale') }}</div>                
+                <img src="https://g.hancel.net/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png">              
             </div>
         </div>
         
