78379fb957662904e3269d9aad85d4309597464a
olsososo <olsososo@gmail.com>
2016-11-18 21:51:04 +0800

    'tpl'

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
-        \App\Http\Middleware::class,
+        \App\Http\Middleware\LocaleMiddle::class,
     ];
 
     /**
