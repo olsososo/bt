141d146f0655a21d65bbe74e48961ed4da4f5076
olsososo <olsososo@gmail.com>
2016-11-19 17:30:26 +0800

    'edit'

--- resources/lang/de/support.php
+++ resources/lang/de/support.php
@@ -1,6 +1,28 @@
 <?php
 
 return [
-    'locale'  =>  'Deutsch',
+    'total_torrents'    =>  'Total Index <span class="text-success">:total</span> magnetische Ressourcen, die kommt aus dem DHT-Netzwerk',
+    'disclaimer'    =>  'Haftungsausschluss',
+    'tutorial'  =>  'Tutorial',
+    'hot'   =>  'Heiß',
+    'footer_disclaimer' =>  'Nur zeigt DHT Netzwerk in Echtzeit und bietet keiner Download BT Samen und Ressource-Dateien',
+    'took'  =>  'Etwa :total Suchergebnisse (:second Sekunden)',
+    'file_size' =>  'Dateigröße',
+    'created_time'  =>  'Zeitpunkt der Erstellung',
+    'update_time'   =>  'Update-Zeit',
+    'resource_heat' =>  'Hitze-Ressource',
+    'magnetic_link' =>  'Magnet-link',
+    'copy' => 'Kopie',
+    'download'  =>  'Download',
+    'details'   =>  'Details',
+    'heat'  =>  'Wärme',
+    'number_of_files'   =>  'Anzahl der Dateien',
+    'keyword'   =>  'Schlüsselwörter',
+    'file_list' =>  'Dateiliste',
+    'file'  =>  'Datei',
+    'size'  =>  'Größe',
+    'search_hint'   =>  'Suche-Magnet-links',
+    'search'    =>  'Suche',
+    'title' =>  ':site_name - BTtorrent-Suchmaschine',
 ];
 
