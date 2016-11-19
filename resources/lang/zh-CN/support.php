4ce81fa77c9ff20dff880a4fd45c5b0209f0cc5b
olsososo <olsososo@gmail.com>
2016-11-19 18:10:59 +0800

    'edit'

--- resources/lang/zh-CN/support.php
+++ resources/lang/zh-CN/support.php
@@ -23,14 +23,14 @@ return [
     'size'  =>  '大小',
     'search_hint'   =>  '搜索磁力链接',
     'search'    =>  '搜索',
-//    'tutorial_content'  =>  '<p><a href=":site_url">:site_name</a>返回的结果是磁力链接，这并不意味着您能通过<a href=":site_url">:site_name</a>直接下载资源，您需要通过下载工具才能实现下载</p>
-//                        <p>目前主流的下载工具有：<a target="_blank" href="http://www.utorrent.com/">μTorrent</a>、<a target="_blank" href="http://www.bitcomet.com/">BitComet</a>等</p>',
-//    'disclaimer_content'    =>  '<p><a href=":site_url">:site_name</a>是一个<a href="https://en.wikipedia.org/wiki/Magnet_URI_scheme" target="_blank">磁力链接</a>搜索引擎，是学术研究的副产品，用于解决资源过度分散的问题</p>
-//                        <p>它通过<a href="https://en.wikipedia.org/wiki/BitTorrent">BitTorrent协议</a>加入DHT网络，实时的自动采集数据，仅存储文件的标题、大小、文件列表、文件标识符（磁力链接）等基础信息</p>
-//                        <p><a href=":site_url">:site_name</a>不下载任何真实资源，无法判断资源的合法性及真实性，使用<a href=":site_url">:site_name</a>服务的用户需自行鉴别内容的真伪</p>
-//                        <p><a href=":site_url">:site_name</a>不上传任何资源，不提供<a href="http://zh.wikipedia.org/zh/BitTorrent_tracker">Tracker服务</a>，不提供<a href="https://en.wikipedia.org/wiki/Torrent_file">种子文件</a>的下载，这意味着<a href=":site_url">:site_name</a>是一个完全合法的系统</p>
-//                        <p>如果您认为有资源信息侵犯了您的权益，请告知本站，在核实信息后，我们将尽快协助处理。 </p>
-//                        <p>如果您搜索到相关宣传恐怖主义，虐童等等反伦理，反道德的资源时，请及时联系我们，我们将删除它。 </p>',
+    'tutorial_content'  =>  '<p><a href=":site_url">:site_name</a>返回的结果是磁力链接，这并不意味着您能通过<a href=":site_url">:site_name</a>直接下载资源，您需要通过下载工具才能实现下载</p>
+                        <p>目前主流的下载工具有：<a target="_blank" href="http://www.utorrent.com/">μTorrent</a>、<a target="_blank" href="http://www.bitcomet.com/">BitComet</a>等</p>',
+    'disclaimer_content'    =>  '<p><a href=":site_url">:site_name</a>是一个<a href="https://en.wikipedia.org/wiki/Magnet_URI_scheme" target="_blank">磁力链接</a>搜索引擎，是学术研究的副产品，用于解决资源过度分散的问题</p>
+                        <p>它通过<a href="https://en.wikipedia.org/wiki/BitTorrent">BitTorrent协议</a>加入DHT网络，实时的自动采集数据，仅存储文件的标题、大小、文件列表、文件标识符（磁力链接）等基础信息</p>
+                        <p><a href=":site_url">:site_name</a>不下载任何真实资源，无法判断资源的合法性及真实性，使用<a href=":site_url">:site_name</a>服务的用户需自行鉴别内容的真伪</p>
+                        <p><a href=":site_url">:site_name</a>不上传任何资源，不提供<a href="http://zh.wikipedia.org/zh/BitTorrent_tracker">Tracker服务</a>，不提供<a href="https://en.wikipedia.org/wiki/Torrent_file">种子文件</a>的下载，这意味着<a href=":site_url">:site_name</a>是一个完全合法的系统</p>
+                        <p>如果您认为有资源信息侵犯了您的权益，请告知本站，在核实信息后，我们将尽快协助处理。 </p>
+                        <p>如果您搜索到相关宣传恐怖主义，虐童等等反伦理，反道德的资源时，请及时联系我们，我们将删除它。 </p>',
     'title' =>  ':site_name - BT种子搜索引擎',
 ];
 
