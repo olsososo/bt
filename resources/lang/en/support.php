6b520098ed1e38eefa3dabe3d4737a610c136672
olsososo <olsososo@gmail.com>
2016-11-19 14:16:22 +0800

    'edit'

--- resources/lang/en/support.php
+++ resources/lang/en/support.php
@@ -25,6 +25,12 @@ return [
     'search_hint'   =>  'search magnet links',
     'search'    =>  'search',
     'tutorial_content'  =>  '<p>The result returned by <a href=":site_url">:site_name</a> is the magnetic link, this does not mean that you can download directly through the <a href=":site_url">:site_name</a>, you need to download a tool to achieve download</p>
-<p>Popular download tools: <a target="_blank" href="http://www.utorrent.com/">μTorrent</a>、<a target="_blank" href="http://www.bitcomet.com/">BitComet</a>, etc</p>',
+                             <p>Popular download tools: <a target="_blank" href="http://www.utorrent.com/">μTorrent</a>、<a target="_blank" href="http://www.bitcomet.com/">BitComet</a>, etc</p>',
+    'disclaimer_content'    =>  '<P> <a href=":site_url">:site_name</a> is a <a href="https://en.wikipedia.org/wiki/Magnet_URI_scheme" target="_blank"> magnetic link </a> search engine, a by-product of academic research , Used to solve the problem of excessive resource dispersion </ p>
+                        <P> It joins the DHT network via the <a href="https://en.wikipedia.org/wiki/BitTorrent"> BitTorrent protocol </a>, capturing data in real time automatically, storing only the file\'s title, size, and file List, file identifiers (magnetic links) and other basic information </ p>
+                        <P> <a href=":site_url">:site_name</a> does not download any real assets and can not determine the legitimacy and authenticity of the resource. Users using the <a href=":site_url">:site_name</a> service are required to authenticate the content themselves. </ P>
+                        <P> <a href=":site_url">:site_name</a> does not upload any resources, does not provide <a href="http://www.wikipedia.org/en/BitTorrent_tracker"> Tracker services </a>, does not provide <a href = " Https://en.wikipedia.org/wiki/Torrent_file "> seed file </a>, which means that <a href=":site_url">:site_name</a> is a fully legitimate system </ p>
+                        <P> If you believe that the resource information infringes your interests, please inform this site, the verification information, we will help deal with as soon as possible. </ P>
+                        <P> If you search for anti-ethical, anti-ethical resources related to the promotion of terrorism, child abuse, etc., please do not hesitate to contact us and we will delete it. </ P>',
 ];
 
--- resources/views/about/statement.blade.php
+++ resources/views/about/statement.blade.php
@@ -16,12 +16,7 @@
                 <div class="col-md-9" role='main'>
                     <div class="bs-docs-section">
                         <h3 id="dropdowns" class="page-header">{{ trans('support.disclaimer') }}</h3>
-                        <p>{{ $site_name }}是一个<a href='http://zh.wikipedia.org/zh/%E7%A3%81%E5%8A%9B%E9%93%BE%E6%8E%A5' target="_blank">磁力链接</a>搜索引擎，是学术研究的副产品，用于解决资源过度分散的问题</p>
-                        <p>它通过<a href="http://zh.wikipedia.org/zh/BitTorrent_(%E5%8D%8F%E8%AE%AE)">BitTorrent协议</a>加入DHT网络，实时的自动采集数据，仅存储文件的标题、大小、文件列表、文件标识符（磁力链接）等基础信息</p>
-                        <p>{{ $site_name }}不下载任何真实资源，无法判断资源的合法性及真实性，使用{{ $site_name }}服务的用户需自行鉴别内容的真伪</p>
-                        <p>{{ $site_name }}不上传任何资源，不提供<a href="http://zh.wikipedia.org/zh/BitTorrent_tracker">Tracker服务</a>，不提供<a href="http://zh.wikipedia.org/wiki/%E7%A7%8D%E5%AD%90%E6%96%87%E4%BB%B6">种子文件</a>的下载，这意味着{{ $site_name }}是一个完全合法的系统</p>
-                        <p>如果您认为有资源信息侵犯了您的权益，请告知本站，在核实信息后，我们将尽快协助处理。 </p>
-                        <p>如果您搜索到相关宣传恐怖主义，虐童等等反伦理，反道德的资源时，请及时联系我们，我们将删除它。 </p>                    
+                        {!! trans('support.disclaimer_content', ['site_name'=>$site_name, 'site_url'=>url('/')]) !!}
                     </div>
                 </div>
             </div>
