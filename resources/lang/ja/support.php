b996a3a8c3774523e6543df6a77a41751b9ca8aa
olsososo <olsososo@gmail.com>
2016-11-19 17:16:14 +0800

    'edit'

--- resources/lang/ja/support.php
+++ resources/lang/ja/support.php
@@ -1,6 +1,28 @@
 <?php
 
 return [
-    'locale'  =>  '日本語',
+    'total_torrents'    =>  '合計インデックス <span class="text-success">:total</span> 磁気リソース、DHT ネットワークからなります。',
+    'disclaimer'    =>  '免責事項',
+    'tutorial'  =>  'チュートリアル',
+    'hot'   =>  'ホット',
+    'footer_disclaimer' =>  'DHT ネットワークをリアルタイムに表示、ダウンロード bt 社の種子やリソース ファイルは提供されません',
+    'took'  =>  '約 :total の結果を見つける (時間: :second 秒)',
+    'file_size' =>  'ファイルのサイズ',
+    'created_time'  =>  '作成時',
+    'update_time'   =>  '更新時間',
+    'resource_heat' =>  '熱資源',
+    'magnetic_link' =>  'マグネット リンク',
+    'copy' => 'コピー',
+    'download'  =>  'ダウンロード',
+    'details'   =>  '詳細',
+    'heat'  =>  '熱',
+    'number_of_files'   =>  'ファイルの数',
+    'keyword'   =>  'キーワード',
+    'file_list' =>  'ファイルの一覧',
+    'file'  =>  'ファイル',
+    'size'  =>  'サイズ',
+    'search_hint'   =>  '検索マグネット リンク',
+    'search'    =>  '検索',
+    'title' =>  ':site_name - BTtorrent 検索エンジン',
 ];
 
