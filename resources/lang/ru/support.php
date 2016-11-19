42be8c988f0af2e32c5205018e1d285623643bdc
olsososo <olsososo@gmail.com>
2016-11-19 17:19:36 +0800

    'edit'

--- resources/lang/ru/support.php
+++ resources/lang/ru/support.php
@@ -1,6 +1,28 @@
 <?php
 
 return [
-    'locale'  =>  'русский',
+    'total_torrents'    =>  'Общий индекс <span class="text-success">:total</span> магнитных ресурсов, который исходит от сети DHT',
+    'disclaimer'    =>  'Отказ от ответственности',
+    'tutorial'  =>  'Учебник',
+    'hot'   =>  'Горячий',
+    'footer_disclaimer' =>  'Только показывать DHT сеть в режиме реального времени и не предоставляет каких-либо скачать BT семена и файлы ресурсов',
+    'took'  =>  '找到约 :total 条结果 （用时 :second 秒） ',
+    'file_size' =>  '文件大小',
+    'created_time'  =>  '创建时间',
+    'update_time'   =>  '更新时间',
+    'resource_heat' =>  '资源热度',
+    'magnetic_link' =>  '磁力链接',
+    'copy' => '复制',
+    'download'  =>  '下载',
+    'details'   =>  '详情',
+    'heat'  =>  '热度',
+    'number_of_files'   =>  '文件数量',
+    'keyword'   =>  '关键词',
+    'file_list' =>  '文件列表',
+    'file'  =>  '文件',
+    'size'  =>  '大小',
+    'search_hint'   =>  '搜索磁力链接',
+    'search'    =>  '搜索',
+    'title' =>  ':site_name - BT种子搜索引擎',
 ];
 
