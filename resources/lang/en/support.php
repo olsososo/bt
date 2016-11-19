e1b00958d194de65eb31daece414072c4b152ac6
olsososo <olsososo@gmail.com>
2016-11-19 11:22:13 +0800

    'edit'

--- resources/lang/en/support.php
+++ resources/lang/en/support.php
@@ -6,6 +6,11 @@ return [
     'disclaimer'    =>  'disclaimer',
     'tutorial'  =>  'tutorial',
     'hot'   =>  'hot',
-    'footer_disclaimer' => 'only showing DHT Network in real time and does not provide any BT seeds and resource files'
+    'footer_disclaimer' => 'only showing DHT Network in real time and does not provide any BT seeds and resource files',
+    'took'  =>  'about :total results found, took :second seconds.',
+    'file_size' =>  'file size',
+    'created_time'  =>  'created time',
+    'update_time'   =>  'update time',
+    'resource_heat' =>  'resource heat',
 ];
 
--- resources/views/index/search.blade.php
+++ resources/views/index/search.blade.php
@@ -15,7 +15,7 @@
             <div class="row">
                 <div class="col-md-9" role='main'>
                     <p>
-                        找到约 {{ number_format($total) }} 条结果 （用时 {{ number_format($running_time, 2) }} 秒）
+                        {{ trans('support.took', ['total'=>number_format($total), 'second'=>number_format($running_time, 2) ]) }}
                     </p>
 
                     <div id="result">
@@ -26,10 +26,10 @@
                                     {!! preg_replace("/$keyword/i", "<span style='color: #dd4b39;'>$keyword</span>", $torrent['name']) !!}</a>
                                 </p>
                                 <span class='st'>
-                                    文件大小: <span class="label label-success" style="margin-right: 10px;">{{ size_format($torrent['length']) }}</span>
-                                    创建时间: <span class="label label-primary" style="margin-right: 10px;">{{ date('Y-m-d', $torrent['created_at']) }}</span>
-                                    更新时间: <span class="label label-warning" style="margin-right: 10px;">{{ date('Y-m-d', $torrent['created_at']) }}</span>
-                                    资源热度: <span class="label label-danger" style="margin-right: 10px;">{{ $torrent['hits'] }}</span>                                
+                                    {{ trans('support.file_size') }}: <span class="label label-success" style="margin-right: 10px;">{{ size_format($torrent['length']) }}</span>
+                                    {{ trans('support.created_time') }}: <span class="label label-primary" style="margin-right: 10px;">{{ date('Y-m-d', $torrent['created_at']) }}</span>
+                                    {{ trans('support.update_time') }}: <span class="label label-warning" style="margin-right: 10px;">{{ date('Y-m-d', $torrent['created_at']) }}</span>
+                                    {{ trans('support.resource_heat') }}: <span class="label label-danger" style="margin-right: 10px;">{{ $torrent['hits'] }}</span>                                
                                 </span>
                             </li>
                             @endforeach
