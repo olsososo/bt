2eae77badcacd5c5d5def0dca679e599cbb398ca
olsososo <olsososo@gmail.com>
2016-11-20 00:55:30 +0800

    'edit'

--- app/Http/Middleware/LocaleMiddle.php
+++ app/Http/Middleware/LocaleMiddle.php
@@ -18,8 +18,6 @@ class LocaleMiddle
      */
     public function handle($request, Closure $next)
     {   
-        var_dump($request->server('HTTP_ACCEPT_LANGUAGE'));
-        
         if (Session::has('locale')) {
             $locale = Session::get('locale', Config::get('app.locale'));
         } else {
