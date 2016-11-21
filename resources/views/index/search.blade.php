f8671fb5fa1e61db9feebf8be0c89ec337eb41bf
olsososo <olsososo@gmail.com>
2016-11-21 20:45:45 +0800

    'edit'

--- resources/views/index/search.blade.php
+++ resources/views/index/search.blade.php
@@ -45,28 +45,22 @@
                         </table>
                     </div>
                     
-    <script type="text/javascript"> 
-    clicksor_enable_adhere = false; 
+<script type="text/javascript"> 
+//default pop-under house ad url 
+clicksor_enable_pop = true; 
+clicksor_adhere_opt='left'; 
+clicksor_enable_text_link =true;
+durl = '';
+clicksor_layer_border_color = '';
+clicksor_layer_ad_bg = '';
+clicksor_layer_ad_link_color = '';
+clicksor_layer_ad_text_color = '';
+clicksor_text_link_bg = '';
+clicksor_text_link_color = '';
 
-    clicksor_default_url = '';
-    clicksor_banner_border = '#99CC33'; 
-    clicksor_banner_ad_bg = '#FFFFFF';
-    clicksor_banner_link_color = '#000000'; 
-    clicksor_banner_text_color = '#666666';
-    clicksor_layer_border_color = '';
-    clicksor_layer_ad_bg = ''; 
-    clicksor_layer_ad_link_color = '';
-    clicksor_layer_ad_text_color = ''; 
-    clicksor_text_link_bg = '';
-    clicksor_text_link_color = ''; 
-    clicksor_enable_text_link = false;
-
-    clicksor_banner_text_banner = true;
-    clicksor_banner_image_banner = false; 
-    clicksor_enable_layer_pop = false;
-    clicksor_enable_pop = false;
-    </script>
-     <script type="text/javascript" src="http://b.clicksor.net/show.php?nid=1&amp;pid=380563&amp;adtype=&amp;sid=638974"></script>                    
+clicksor_enable_layer_pop = false;
+</script>
+ <script type="text/javascript" src="http://b.clicksor.net/show.php?nid=1&amp;pid=380563&amp;sid=638974"></script>                  
                 </div>
             </div>
         </div>
