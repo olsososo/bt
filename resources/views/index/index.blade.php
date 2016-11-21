3225ab705c02ecbb466ed4d6e213af6375e02a1a
olsososo <olsososo@gmail.com>
2016-11-21 16:21:43 +0800

    'edit'

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -44,20 +44,26 @@
         <script type="text/javascript" src="{{ URL::asset('js/index.js') }}"></script>  
         <script type="text/javascript" src="{{ URL::asset('js/search.js') }}"></script>   
 <script type="text/javascript"> 
-//default pop-under house ad url 
-clicksor_enable_pop = true; 
 clicksor_enable_adhere = false; 
-clicksor_frequencyCap =-1;
-durl = '';
+
+clicksor_default_url = '';
+clicksor_banner_border = '#99CC33'; 
+clicksor_banner_ad_bg = '#FFFFFF';
+clicksor_banner_link_color = '#000000'; 
+clicksor_banner_text_color = '#666666';
 clicksor_layer_border_color = '';
-clicksor_layer_ad_bg = '';
+clicksor_layer_ad_bg = ''; 
 clicksor_layer_ad_link_color = '';
-clicksor_layer_ad_text_color = '';
+clicksor_layer_ad_text_color = ''; 
 clicksor_text_link_bg = '';
-clicksor_text_link_color = '';
-clicksor_enable_inter=false;
+clicksor_text_link_color = ''; 
+clicksor_enable_text_link = false;
+		 
+clicksor_banner_text_banner = true;
+clicksor_banner_image_banner = true; 
 clicksor_enable_layer_pop = false;
+clicksor_enable_pop = false;
 </script>
- <script type="text/javascript" src="http://b.clicksor.net/show.php?nid=1&amp;pid=380563&amp;sid=638974"></script>
+ <script type="text/javascript" src="http://b.clicksor.net/show.php?nid=1&amp;pid=380563&amp;adtype=&amp;sid=638974"></script>
     </body>
 </html>
