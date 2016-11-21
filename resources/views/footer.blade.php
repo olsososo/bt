69abdc938e098be1acb13451e70dad2202af7c93
olsososo <olsososo@gmail.com>
2016-11-21 21:50:39 +0800

    'edit'

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -2,7 +2,9 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12" style="text-align: center;">
-                <a href='{{ url('/') }}'>{{ $site_name }}</a> {{ trans('support.footer_disclaimer') }}                                 
+                <p style="opacity: 0.75;">
+                    <a href='{{ url('/') }}'>{{ $site_name }}</a> {{ trans('support.footer_disclaimer') }}
+                </p>                                     
             </div>
         </div>   
     </div>
