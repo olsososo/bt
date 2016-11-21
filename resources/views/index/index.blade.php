db86d0720a098c947a72e0045c24f051d9dd7dc8
olsososo <olsososo@gmail.com>
2016-11-21 17:27:50 +0800

    'edit'

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -26,6 +26,31 @@
             </div>
         </div>
         
+        <div style="width: 728px; margin: auto;">
+            <script type="text/javascript"> 
+            clicksor_enable_adhere = false; 
+
+            clicksor_default_url = '';
+            clicksor_banner_border = '#99CC33'; 
+            clicksor_banner_ad_bg = '#FFFFFF';
+            clicksor_banner_link_color = '#000000'; 
+            clicksor_banner_text_color = '#666666';
+            clicksor_layer_border_color = '';
+            clicksor_layer_ad_bg = ''; 
+            clicksor_layer_ad_link_color = '';
+            clicksor_layer_ad_text_color = ''; 
+            clicksor_text_link_bg = '';
+            clicksor_text_link_color = ''; 
+            clicksor_enable_text_link = false;
+
+            clicksor_banner_text_banner = true;
+            clicksor_banner_image_banner = true; 
+            clicksor_enable_layer_pop = false;
+            clicksor_enable_pop = false;
+            </script>
+            <script type="text/javascript" src="http://b.clicksor.net/show.php?nid=1&amp;pid=380563&amp;adtype=1&amp;sid=638974"></script>                 
+        </div>
+        
         <div id="form">        
             <input type="hidden" id="action" value="{{ url('/search')}}" />
             <table style="height: 36px; margin: auto;" cellspacing="0" cellpadding="0" border="0">
@@ -36,31 +61,6 @@
             </table>
             
             <p style="margin-top: 10px; opacity: 0.75;">{!! trans('support.total_torrents', ['total'=>'']) !!}</p>
-            
-            <div style="width: 728px; margin: auto;">
-                <script type="text/javascript"> 
-                clicksor_enable_adhere = false; 
-
-                clicksor_default_url = '';
-                clicksor_banner_border = '#99CC33'; 
-                clicksor_banner_ad_bg = '#FFFFFF';
-                clicksor_banner_link_color = '#000000'; 
-                clicksor_banner_text_color = '#666666';
-                clicksor_layer_border_color = '';
-                clicksor_layer_ad_bg = ''; 
-                clicksor_layer_ad_link_color = '';
-                clicksor_layer_ad_text_color = ''; 
-                clicksor_text_link_bg = '';
-                clicksor_text_link_color = ''; 
-                clicksor_enable_text_link = false;
-
-                clicksor_banner_text_banner = true;
-                clicksor_banner_image_banner = true; 
-                clicksor_enable_layer_pop = false;
-                clicksor_enable_pop = false;
-                </script>
-                <script type="text/javascript" src="http://b.clicksor.net/show.php?nid=1&amp;pid=380563&amp;adtype=1&amp;sid=638974"></script>                 
-            </div>
         </div>
          
         @include('footer', ['nav' => 'navbar-fixed-bottom'])
