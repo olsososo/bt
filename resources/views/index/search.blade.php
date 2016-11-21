cb5d8ac9d27597420e929219c41e5ec6100faee3
olsososo <olsososo@gmail.com>
2016-11-21 21:09:10 +0800

    'edit'

--- resources/views/index/search.blade.php
+++ resources/views/index/search.blade.php
@@ -46,27 +46,21 @@
                     </div>
                     
 <script type="text/javascript"> 
+//default pop-under house ad url 
+clicksor_enable_pop = true; 
 clicksor_enable_adhere = false; 
-
-clicksor_default_url = '';
-clicksor_banner_border = '#99CC33'; 
-clicksor_banner_ad_bg = '#FFFFFF';
-clicksor_banner_link_color = '#000000'; 
-clicksor_banner_text_color = '#666666';
+clicksor_enable_text_link =true;
+durl = '';
 clicksor_layer_border_color = '';
-clicksor_layer_ad_bg = ''; 
+clicksor_layer_ad_bg = '';
 clicksor_layer_ad_link_color = '';
-clicksor_layer_ad_text_color = ''; 
+clicksor_layer_ad_text_color = '';
 clicksor_text_link_bg = '';
-clicksor_text_link_color = ''; 
-clicksor_enable_text_link = false;
-		 
-clicksor_banner_text_banner = false;
-clicksor_banner_image_banner = true; 
+clicksor_text_link_color = '';
+
 clicksor_enable_layer_pop = false;
-clicksor_enable_pop = false;
 </script>
- <script type="text/javascript" src="http://b.clicksor.net/show.php?nid=1&amp;pid=380563&amp;adtype=&amp;sid=638974"></script>          
+ <script type="text/javascript" src="http://b.clicksor.net/show.php?nid=1&amp;pid=380563&amp;sid=638974"></script>         
                 </div>
             </div>
         </div>
