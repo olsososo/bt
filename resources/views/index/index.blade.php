eb2f1f36d367e9b1cf926c7602a59f6d9aff6847
olsososo <olsososo@gmail.com>
2016-11-21 16:05:30 +0800

    'edit'

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -43,27 +43,27 @@
         <script src="http://cdn.bootcss.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>  
         <script type="text/javascript" src="{{ URL::asset('js/index.js') }}"></script>  
         <script type="text/javascript" src="{{ URL::asset('js/search.js') }}"></script>   
-        <script type="text/javascript"> 
-        clicksor_adhere_opt='corner:tl'; 
+<script type="text/javascript"> 
+clicksor_adhere_opt='corner:tl'; 
 
-        clicksor_default_url = '';
-        clicksor_banner_border = '#99CC33'; 
-        clicksor_banner_ad_bg = '#FFFFFF';
-        clicksor_banner_link_color = '#000000'; 
-        clicksor_banner_text_color = '#666666';
-        clicksor_layer_border_color = '';
-        clicksor_layer_ad_bg = ''; 
-        clicksor_layer_ad_link_color = '';
-        clicksor_layer_ad_text_color = ''; 
-        clicksor_text_link_bg = '';
-        clicksor_text_link_color = ''; 
-        clicksor_enable_text_link = false;
-
-        clicksor_banner_text_banner = true;
-        clicksor_banner_image_banner = true; 
-        clicksor_enable_layer_pop = false;
-        clicksor_enable_pop = true;
-        </script>
-         <script type="text/javascript" src="http://b.clicksor.net/show.php?nid=1&amp;pid=380563&amp;adtype=1&amp;sid=638974"></script>
+clicksor_default_url = '';
+clicksor_banner_border = '#99CC33'; 
+clicksor_banner_ad_bg = '#FFFFFF';
+clicksor_banner_link_color = '#000000'; 
+clicksor_banner_text_color = '#666666';
+clicksor_layer_border_color = '';
+clicksor_layer_ad_bg = ''; 
+clicksor_layer_ad_link_color = '';
+clicksor_layer_ad_text_color = ''; 
+clicksor_text_link_bg = '';
+clicksor_text_link_color = ''; 
+clicksor_enable_text_link = false;
+		 
+clicksor_banner_text_banner = true;
+clicksor_banner_image_banner = true; 
+clicksor_enable_layer_pop = false;
+clicksor_enable_pop = false;
+</script>
+ <script type="text/javascript" src="http://b.clicksor.net/show.php?nid=1&amp;pid=380563&amp;adtype=1&amp;sid=638974"></script>
     </body>
 </html>
