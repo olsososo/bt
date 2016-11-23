b6016e1baa394df71688afa50f3d3b96f4b58169
olsososo <olsososo@gmail.com>
2016-11-23 17:16:28 +0800

    'edit'

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -9,3 +9,16 @@
         </div>   
     </div>
 </nav>
+
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-53246231-3', 'auto');
+    ga('send', 'pageview');
+</script>
+
+<!-- Go to www.addthis.com/dashboard to customize your tools -->
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-58355dd47c068b12"></script>
