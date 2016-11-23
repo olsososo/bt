2c0f6f2ab23f48becb122cae16848720086125c8
olsososo <olsososo@gmail.com>
2016-11-24 00:52:37 +0800

    'edit'

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,16 @@
+<div class="content">
+    <div class="title">Something went wrong.</div>
+    @unless(empty($sentryID))
+        <!-- Sentry JS SDK 2.1.+ required -->
+        <script src="https://cdn.ravenjs.com/3.3.0/raven.min.js"></script>
+
+        <script>
+        Raven.showReportDialog({
+            eventId: '{{ $sentryID }}',
+
+            // use the public DSN (dont include your secret!)
+            dsn: 'https://d395d719101b46c4bef72a3a8c91f5e5@sentry.io/116934'
+        });
+        </script>
+    @endunless
+</div>
