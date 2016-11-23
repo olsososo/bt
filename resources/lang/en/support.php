65e8769f1e175357cdf259334843b12c11f0a307
olsososo <olsososo@gmail.com>
2016-11-24 01:41:04 +0800

    'edit'

--- resources/lang/en/support.php
+++ resources/lang/en/support.php
@@ -26,7 +26,7 @@ return [
     'tutorial_content'  =>  '<p>The result returned by <a href=":site_url">:site_name</a> is the magnet link, this does not mean that you can download directly through the <a href=":site_url">:site_name</a>, you need to download a tool to achieve download</p>
                              <p>Popular download tools: <a target="_blank" href="http://www.utorrent.com/">μTorrent</a>、<a target="_blank" href="http://www.bitcomet.com/">BitComet</a>, etc</p>',
     'disclaimer_content'    =>  
-                       '<p> <a href=":site_url">:site_name</a> is a torrent search engine, by-products of academic research, Used solve the problem of excessive fragmentation of resources</p>
+                       '<p> <a href=":site_url">:site_name</a> is a torrent search engine, by-products of academic research, used solve the problem of excessive fragmentation of resources</p>
                         <p> It joins DHT network via <a href="https://en.wikipedia.org/wiki/BitTorrent">BitTorrent protocol</a>, collect the data automatically in real time, store the basic information such as the title, size, file list, file identifier only</p>
                         <p> <a href=":site_url">:site_name</a> do not download any real resources, unable to judge the legitimacy and authenticity of resources, you need to identify the authenticity of content</a>
                         <p> <a href=":site_url">:site_name</a> do not upload any resources, do not provide <a href="http://www.wikipedia.org/en/BitTorrent_tracker"> Tracker services </a>, do not provide <a href = "https://en.wikipedia.org/wiki/Torrent_file">torrent file</a> download, which means that it is a fully legal system</p>
