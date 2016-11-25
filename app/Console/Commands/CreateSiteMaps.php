ca551b801121f1d0622be329705009b2f49ce1fd
olsososo <olsososo@gmail.com>
2016-11-25 22:18:42 +0800

    'edit'

--- app/Console/Commands/CreateSiteMaps.php
+++ app/Console/Commands/CreateSiteMaps.php
@@ -64,7 +64,7 @@ class CreateSiteMaps extends Command
                 $url->addChild('loc', 'http://www.ibittorrent.com/torrent/'.  base64_encode($value->id));
                 $url->addChild('lastmod', date('Y-m-d', $value->updated_at));
              
-                $hander = fopen($path, 'w');
+                $handle = fopen($path, 'w');
                 fwrite($handle, (string)$xml);
                 fclose($handle);
             }
