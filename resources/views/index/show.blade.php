68ef1668aad7aafc92856fbb9ded76eb07badabb
olsososo <olsososo@gmail.com>
2016-11-26 18:07:05 +0800

    'bug'

--- resources/views/index/show.blade.php
+++ resources/views/index/show.blade.php
@@ -78,7 +78,29 @@
                         @endif
                         </tbody>
                     </table>
+                    
+                    <div id="disqus_thread"></div>
+                    <script>
+
+                    /**
+                    *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+                    *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+                    /*
+                    var disqus_config = function () {
+                    this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+                    this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+                    };
+                    */
+                    (function() { // DON'T EDIT BELOW THIS LINE
+                    var d = document, s = d.createElement('script');
+                    s.src = '//ibittorrent.disqus.com/embed.js';
+                    s.setAttribute('data-timestamp', +new Date());
+                    (d.head || d.body).appendChild(s);
+                    })();
+                    </script>
+                    <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>                    
                 </div>
+                
                 <div class="col-md-3" role='main'>
                     <div style="margin-top: 40px;">
                         @if ($env == 'production')
@@ -106,7 +128,7 @@
                         <script type="text/javascript" src="http://b.clicksor.net/show.php?nid=1&amp;pid=380563&amp;adtype=8&amp;sid=638974"></script>
                         @endif
                     </div>
-                </div>
+                </div>                                                
             </div>         
         </div>
         
