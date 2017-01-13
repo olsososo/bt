32fd72c76c62e1180780b9b375aebfddd899f287
olsososo <olsososo@gmail.com>
2017-01-14 04:01:00 +0800

    fix bug

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('sitemaps:create')->hourly();
+        $schedule->command('sitemaps:create')->daily();
     }
 
     /**
