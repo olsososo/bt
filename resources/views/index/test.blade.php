bcf783c8b2ee10ab4098e9370e3bd0ca810a5830
olsososo <olsososo@gmail.com>
2017-01-20 05:29:50 +0800

    fix bug

--- resources/views/index/test.blade.php
+++ resources/views/index/test.blade.php
@@ -10,43 +10,17 @@
         <link rel="stylesheet" href="{{ URL::asset('css/index.css') }}" > 
     </head>
     <body>
-<header class="navbar navbar-static-top bs-docs-nav" id="top" role="banner">
-  <div class="container">
-    <div class="navbar-header">
-      <button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a href="../" class="navbar-brand">Bootstrap</a>
-    </div>
-    <nav class="collapse navbar-collapse bs-navbar-collapse" role="navigation">
-      <ul class="nav navbar-nav">
-        <li>
-          <a href="../getting-started/">起步</a>
-        </li>
-        <li class="active">
-          <a href="../css/">全局 CSS 样式</a>
-        </li>
-        <li>
-          <a href="../components/">组件</a>
-        </li>
-        <li>
-          <a href="../javascript/">JavaScript 插件</a>
-        </li>
-        <li>
-          <a href="../customize/">定制</a>
-        </li>
-      </ul>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="http://job.bootcss.com" onclick="_hmt.push(['_trackEvent', 'docv3-navbar', 'click', 'doc-home-navbar-job'])" target="_blank">高薪工作</a></li>
-        <li><a href="http://expo.bootcss.com" onclick="_hmt.push(['_trackEvent', 'docv3-navbar', 'click', 'doc-home-navbar-expo'])" target="_blank">优站精选</a></li>
-        <li><a href="http://blog.getbootstrap.com" onclick="_hmt.push(['_trackEvent', 'docv3-navbar', 'click', 'doc-home-navbar-blog'])" target="_blank">官方博客</a></li>
-      </ul>
-    </nav>
-  </div>
-</header>
+        <header class="navbar navbar-static-top bs-docs-nav" id="top" role="banner">
+          <div class="container">
+            <nav class="collapse navbar-collapse bs-navbar-collapse" role="navigation">
+              <ul class="nav navbar-nav navbar-right">
+                <li><a href="{{ URL::route('disclaimer') }}" onclick="_hmt.push(['_trackEvent', 'docv3-navbar', 'click', 'doc-home-navbar-job'])" target="_blank">{{ trans('support.disclaimer') }}</a></li>
+                <li><a href="{{ URL::route('tutorial') }}" onclick="_hmt.push(['_trackEvent', 'docv3-navbar', 'click', 'doc-home-navbar-expo'])" target="_blank">{{ trans('support.tutorial') }}</a></li>
+                <li><a href="{{ URL::route('hot', ['date'=>strtotime(date('Y-m-d'))]) }}" onclick="_hmt.push(['_trackEvent', 'docv3-navbar', 'click', 'doc-home-navbar-blog'])" target="_blank">{{ trans('support.hot') }}</a></li>
+              </ul>
+            </nav>
+          </div>
+        </header>
         
         <div id="logo">
             <div id="logo-sub">
