2c186284c53a880414d12d20465a68e35c91ddf0
olsososo <olsososo@gmail.com>
2017-01-21 03:58:53 +0800

    fix bug

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -7,36 +7,45 @@
         <meta name="description" content=""/>
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <link rel="stylesheet" href="{{ URL::asset('css/bootstrap.min.css') }}" >
-        <link rel="stylesheet" href="{{ URL::asset('css/index.css') }}" > 
+        <link rel="stylesheet" href="{{ URL::asset('css/test.css') }}" > 
     </head>
     <body>
-        <div id="header">
-            <div id="menu">
-                <ul>
-                    <li><a href="{{ URL::route('disclaimer') }}">{{ trans('support.disclaimer') }}</a></li>
-                    <li><a href="{{ URL::route('tutorial') }}">{{ trans('support.tutorial') }}</a></li>
-                    <li><a href="{{ URL::route('hot', ['date'=>strtotime(date('Y-m-d'))]) }}">{{ trans('support.hot') }}</a></li>
-                </ul>             
+        <div class="container-fluid">
+            <nav class="collapse navbar-collapse bs-navbar-collapse" role="navigation">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="{{ URL::route('disclaimer') }}" onclick="_hmt.push(['_trackEvent', 'docv3-navbar', 'click', 'doc-home-navbar-job'])" target="_blank">{{ trans('support.disclaimer') }}</a></li>
+                    <li><a href="{{ URL::route('tutorial') }}" onclick="_hmt.push(['_trackEvent', 'docv3-navbar', 'click', 'doc-home-navbar-expo'])" target="_blank">{{ trans('support.tutorial') }}</a></li>
+                    <li><a href="{{ URL::route('hot', ['date'=>strtotime(date('Y-m-d'))]) }}" onclick="_hmt.push(['_trackEvent', 'docv3-navbar', 'click', 'doc-home-navbar-blog'])" target="_blank">{{ trans('support.hot') }}</a></li>
+                </ul>
+            </nav>
+        </div>
+        
+        <div id="logo" class="container-fluid">
+            <div class="row">
+                <div class="col-xs-4 col-xs-offset-4 col-md-4 col-md-offset-4">
+                    <img src="{{ URL::asset('images/logo.png') }}" >
+                </div>
             </div>
         </div>
         
-        <div id="logo">
-            <div id="logo-sub">
-                <img src="{{ URL::asset('images/logo.png') }}" >
+        <div class="container-fluid" id="form">
+            <div class="row">
+                <div class="col-xs-8 col-xs-offset-2 col-md-6 col-md-offset-3">
+                    <form role="form">
+                        <div class="input-group">
+                            <input type="text" class="form-control" id="keyword" placeholder="{{ trans('support.search_hint') }}">
+                            <span class="input-group-btn">
+                                <button class="btn btn-default" type="button" id="search">Search</button>
+                            </span>
+                        </div>
+                        <input type="hidden" id="action" value="{{ url('/search')}}" />
+                    </form>
+                    
+                    <p>{!! trans('support.total_torrents', ['total'=>'']) !!}</p>
+                </div>
             </div>
         </div>
-        
-        <div id="form">        
-            <input type="hidden" id="action" value="{{ url('/search')}}" />
-            <table style="height: 36px; margin: auto;" cellspacing="0" cellpadding="0" border="0">
-                <tr>
-                    <td><input type="text" id="keyword" placeholder="{{ trans('support.search_hint') }}" /></td>
-                    <td><input id="search" type="button" value="{{ trans('support.search') }}" /></td>
-                </tr>
-            </table>
-            
-            <p style="margin-top: 10px; opacity: 0.75;">{!! trans('support.total_torrents', ['total'=>'']) !!}</p>
-            
+         
             <div style="width: 728px; margin: 20px auto;">
                 @if ($env == 'production')
 <script type="text/javascript"> 
@@ -64,7 +73,7 @@ clicksor_enable_pop = false;
                 @endif
             </div>
         </div>
-         
+        
         @include('footer', ['nav' => 'navbar-fixed-bottom'])
         
         <script type="text/javascript" src="{{ URL::asset('js/jquery.min.js') }}"></script>
