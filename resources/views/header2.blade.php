a8b4f8ee8c09ef69b9a3f19cc0c7b54a18949b82
olsososo <olsososo@gmail.com>
2017-01-23 05:32:08 +0800

    fix bug

--- resources/views/header2.blade.php
+++ resources/views/header2.blade.php
@@ -7,14 +7,15 @@
 
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <div class="input-group navbar-form navbar-left">
-                <input type="text" class="form-control" id="keyword" placeholder="{{ trans('support.search_hint') }}">
-                <span class="input-group-btn">
-                    <button class="btn btn-default" type="button" id="search">Search</button>
-                </span>
-            </div>
-            <input type="hidden" id="action" value="{{ url('/search')}}" />
-
+            <form class="navbar-form navbar-left" role="search">
+                <div class="input-group">
+                    <input type="text" class="form-control" id="keyword" placeholder="{{ trans('support.search_hint') }}">
+                    <span class="input-group-btn">
+                        <button class="btn btn-default" type="button" id="search">Search</button>
+                    </span>
+                </div>
+                <input type="hidden" id="action" value="{{ url('/search')}}" />
+            </form>
             <ul class="nav navbar-nav navbar-right">
                 <li><a href="{{ URL::route('disclaimer') }}">{{ trans('support.disclaimer') }}</a></li>
                 <li><a href="{{ URL::route('tutorial') }}">{{ trans('support.tutorial') }}</a></li>
