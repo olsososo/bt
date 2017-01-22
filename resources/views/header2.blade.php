8eee0ac99bfd53653e90b6adb7f7a6c69d3535b7
olsososo <olsososo@gmail.com>
2017-01-23 00:40:03 +0800

    fix bug

--- resources/views/header2.blade.php
+++ resources/views/header2.blade.php
@@ -1,28 +1,37 @@
-        <div id="header">
-           <div id="left_menu">
-                <table cellspacing="0" cellpadding="0">
-                    <tr>
-                        <td id="logo">
-                            <a href="{{ URL::route('index') }}"><img src="{{ URL::asset('images/logo.png') }}" /></a>
-                        </td>
-                        <td>
-                            <input type="hidden" id="action" value="{{ url('/search')}}" />
-                            <input type="text" id="keyword" placeholder="{{ trans('support.search_hint') }}" value="{{ $keyword or '' }}"/>
-                        </td>
-                        <td>
-                            <span id="search_spn">
-                                <input id="search" type="button" value="{{ trans('support.search') }}" />
-                            </span>
-                        </td>
-                    </tr>
-                </table>
-            </div>
-            
-            <div id="menu">
-                <ul>
-                    <li><a href="{{ URL::route('disclaimer') }}">{{ trans('support.disclaimer') }}</a></li>
-                    <li><a href="{{ URL::route('tutorial') }}">{{ trans('support.tutorial') }}</a></li>
-                    <li><a href="{{ URL::route('hot', ['date'=>strtotime(date('Y-m-d'))]) }}">{{ trans('support.hot') }}</a></li>
-                </ul>             
-            </div>
+<header class="navbar navbar-static-top bs-docs-nav" id="top" role="banner">
+    <div class="container">
+        <div class="navbar-header">
+            <button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a href="../" class="navbar-brand">Bootstrap</a>
         </div>
+        <nav class="collapse navbar-collapse bs-navbar-collapse" role="navigation">
+            <ul class="nav navbar-nav">
+                <li>
+                  <a href="../getting-started/">起步</a>
+                </li>
+                <li class="active">
+                  <a href="../css/">全局 CSS 样式</a>
+                </li>
+                <li>
+                  <a href="../components/">组件</a>
+                </li>
+                <li>
+                  <a href="../javascript/">JavaScript 插件</a>
+                </li>
+                <li>
+                  <a href="../customize/">定制</a>
+                </li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="http://job.bootcss.com" onclick="_hmt.push(['_trackEvent', 'docv3-navbar', 'click', 'doc-home-navbar-job'])" target="_blank">高薪工作</a></li>
+                <li><a href="http://expo.bootcss.com" onclick="_hmt.push(['_trackEvent', 'docv3-navbar', 'click', 'doc-home-navbar-expo'])" target="_blank">优站精选</a></li>
+                <li><a href="http://blog.getbootstrap.com" onclick="_hmt.push(['_trackEvent', 'docv3-navbar', 'click', 'doc-home-navbar-blog'])" target="_blank">官方博客</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
