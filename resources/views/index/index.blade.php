05e1f7bdf44fdccba2de06056131b000b79432f8
olsososo <olsososo@gmail.com>
2017-01-22 23:47:46 +0800

    fix bug

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -43,33 +43,6 @@
                 </div>
             </div>
         </div>
-         
-        <div style="width: 728px; margin: 20px auto;">
-            @if ($env == 'production')
-<script type="text/javascript"> 
-clicksor_enable_adhere = false; 
-
-clicksor_default_url = '';
-clicksor_banner_border = '#99CC33'; 
-clicksor_banner_ad_bg = '#FFFFFF';
-clicksor_banner_link_color = '#000000'; 
-clicksor_banner_text_color = '#666666';
-clicksor_layer_border_color = '';
-clicksor_layer_ad_bg = ''; 
-clicksor_layer_ad_link_color = '';
-clicksor_layer_ad_text_color = ''; 
-clicksor_text_link_bg = '';
-clicksor_text_link_color = ''; 
-clicksor_enable_text_link = false;
-
-clicksor_banner_text_banner = false;
-clicksor_banner_image_banner = true; 
-clicksor_enable_layer_pop = false;
-clicksor_enable_pop = false;
-</script>
-<script type="text/javascript" src="http://b.clicksor.net/show.php?nid=1&amp;pid=380563&amp;adtype=1&amp;sid=639195"></script>
-            @endif
-        </div>
         
         @include('footer', ['nav' => 'navbar-fixed-bottom'])
         
