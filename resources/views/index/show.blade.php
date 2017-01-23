4a42597b60f3950c384f0715721e939bdec445d6
olsososo <olsososo@gmail.com>
2017-01-24 01:58:50 +0800

    fix bug

--- resources/views/index/show.blade.php
+++ resources/views/index/show.blade.php
@@ -12,9 +12,9 @@
     <body>
         @include('header')
         
-        <div class="container">
+        <div class="container-fluid">
             <div class="row">
-                <div class="col-md-9" role='main'>
+                <div class="col-md-9 col-md-offset-1" role='main'>
                     <h3 id="dropdowns" class="page-header">{{ trans('support.magnetic_link') }}</h3>
                     <p>
                         <a class="title" href="{{ URL::route('show', ['id'=>base64_encode($torrent['id'])]) }}">
@@ -100,36 +100,7 @@
                     })();
                     </script>
                     <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>                    
-                </div>
-                
-                <div class="col-md-3" role='main'>
-                    <div style="margin-top: 40px;">
-                        @if ($env == 'production')
-                        <script type="text/javascript"> 
-                        clicksor_enable_adhere = false; 
-
-                        clicksor_default_url = '';
-                        clicksor_banner_border = '#99CC33'; 
-                        clicksor_banner_ad_bg = '#FFFFFF';
-                        clicksor_banner_link_color = '#000000'; 
-                        clicksor_banner_text_color = '#666666';
-                        clicksor_layer_border_color = '';
-                        clicksor_layer_ad_bg = ''; 
-                        clicksor_layer_ad_link_color = '';
-                        clicksor_layer_ad_text_color = ''; 
-                        clicksor_text_link_bg = '';
-                        clicksor_text_link_color = ''; 
-                        clicksor_enable_text_link = false;
-
-                        clicksor_banner_text_banner = false;
-                        clicksor_banner_image_banner = true; 
-                        clicksor_enable_layer_pop = false;
-                        clicksor_enable_pop = false;
-                        </script>
-                        <script type="text/javascript" src="http://b.clicksor.net/show.php?nid=1&amp;pid=380563&amp;adtype=8&amp;sid=638974"></script>
-                        @endif
-                    </div>
-                </div>                                                
+                </div>                                               
             </div>         
         </div>
         
