f0a4e7afccef7a178785e0129e416635d51191e4
olsososo <olsososo@gmail.com>
2017-02-01 04:24:50 +0800

    fix bug

--- resources/views/index/show.blade.php
+++ resources/views/index/show.blade.php
@@ -124,7 +124,7 @@ clicksor_banner_image_banner = true;
 clicksor_enable_layer_pop = false;
 clicksor_enable_pop = false;
 </script>
- <script type="text/javascript" src="http://b.clicksor.net/show.php?nid=1&amp;pid=380563&amp;adtype=8&amp;sid=638974"></script>                  
+ <script type="text/javascript" src="http://b.clicksor.net/show.php?nid=1&amp;pid=380563&amp;adtype=6&amp;sid=638974"></script>                
                 </div>
             </div>         
         </div>
