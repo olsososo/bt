456bdc370f5d62bcdd4ac4ce2a0757e528287207
olsososo <olsososo@gmail.com>
2017-02-07 01:55:52 +0800

    fix bug

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         @include('verification')
         <title>{{ trans('support.title', ['site_name'=>$site_name]) }}</title>
+        <meta name=viewport content="width=device-width, initial-scale=1">
         <meta name="description" content=""/>
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <link rel="stylesheet" href="{{ URL::asset('css/bootstrap.min.css') }}" >
