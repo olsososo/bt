283f7619e8abb9c169c05126f3b0d50884fbe404
olsososo <olsososo@gmail.com>
2017-02-07 02:03:06 +0800

    fix bug

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -23,7 +23,7 @@
         
         <div id="logo" class="container-fluid">
             <div class="row">
-                <div class="col-xs-6 col-xs-offset-3 col-md-4 col-md-offset-4">
+                <div class="col-xs-8 col-xs-offset-2 col-md-4 col-md-offset-4">
                     <img src="{{ URL::asset('images/logo.png') }}" >
                 </div>
             </div>
